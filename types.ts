abb1032eea48affd938dd4312cee0af570bfcc1e
fkiptooh <fkiptooh@gmail.com>
2023-11-27 01:24:56 +0300

    upsert product and price

--- types.ts
+++ types.ts
@@ -31,11 +31,11 @@ export interface Product {
 export interface Price {
     id: string;
     product_id?: string;
-    active?: string;
+    active?: boolean;
     description?: string;
-    unit_amount?: string;
+    unit_amount?: number;
     currency?: string;
-    types?: Stripe.Price.Type;
+    type?: Stripe.Price.Type;
     interval?: Stripe.Price.Recurring.Interval;
     interval_count?: number;
     trial_period_days?: number | null;
